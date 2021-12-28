032c4445cc70f2f828f7035ed179767bf3fcc9dc
EvertonAdame <87421330+EvertonAdame@users.noreply.github.com>
2021-12-28 19:04:27 -0300

    teste44

--- src/styles/pages/TeacherDropDown/index.tsx
+++ src/styles/pages/TeacherDropDown/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { FaEllipsisV } from 'react-icons/fa';
 // Completed
-
-import { Container, StyledButton } from './styles';
+import { Container } from './styles';
 
 export const DropdownMenu = ({
   isLoading = false,
